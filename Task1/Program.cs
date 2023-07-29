0056c3de7c93955c32489834dd1b35ff5795abb8
Anton <antontokmakov757@gmail.com>
2023-07-29 15:08:29 +0300

    Example complited

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите массив чисел");
+int[] mas = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+
+Console.WriteLine($"Колличество всех элементов: {mas.Length} \nКолличество элементов > 0: {CountPolItem()}");
+
+int CountPolItem()
+{
+    int count = 0;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
